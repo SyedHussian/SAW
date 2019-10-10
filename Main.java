d97a018202b3696d1c148e1b96f436018b0c9a65
SyedHussain <tah94mid@gmail.com>
2019-10-10 13:18:34 -0400

    3

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+package sawr;
 
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -8,7 +9,6 @@ public class Main {
 
 	public static CopyOnWriteArrayList<Double> rSquaredSum; 
 	public static AtomicInteger N_SAW_TOT; 
-	public static AtomicInteger N_SAP_TOT; 
 	static double sumOfAverages; 
 	static double rsqavg;
 
@@ -17,68 +17,135 @@ public class Main {
 		boolean br = true;
 
 		while (br) {
-		System.out.println("Please enter 2 for 2D, 3 for 3D, 4 for 4D and 5 for 2D Polygon");
-		int dimension = input.nextInt();
-		System.out.println("n\tR^2\t\t\t\tF_SAW");
+		System.out.println("Please enter d for dimension or p for polygon");
+		String choice = input.next();
 		
-		
-			if (dimension == 2) {
-				Thread arrThreads2D[] = new Thread[Globals.N_T];
-	
-				for (int n = 10; n <= Globals.max_steps; n++) {
-
-					rSquaredSum = new CopyOnWriteArrayList<Double>();
-					N_SAW_TOT = new AtomicInteger();
-					sumOfAverages = 0;
-					
-					for (int i=0; i<Globals.N_T; i++) {
-						Thread t1 = new Thread(new Dimensions(dimension, n));
-						t1.start();
-						arrThreads2D[i] = t1;
-					}
-					for (int i = 0; i < Globals.N_T; i++) {
-						arrThreads2D[i].join();
-					}
-					print(n);
-				}
-			}
-			else if (dimension == 3) {
-				Thread arrThreads3D[] = new Thread[Globals.N_T];
-	
-				for (int n = 10; n <= Globals.max_steps; n++) {
-					rSquaredSum = new CopyOnWriteArrayList<Double>();
-					N_SAW_TOT = new AtomicInteger();
-					sumOfAverages = 0;
-					
-					for (int i=0; i<Globals.N_T; i++) {
-						Thread t1 = new Thread(new Dimensions(dimension, n));
-						t1.start();
-						arrThreads3D[i] = t1;
-					}
-					for (int i = 0; i < Globals.N_T; i++) {
-						arrThreads3D[i].join();
-					}
-					print(n);
-				}
-			}
-			else if (dimension == 4) {
-				Thread arrThreads4D[] = new Thread[Globals.N_T];
+			if (choice.equalsIgnoreCase("d")) {
+				System.out.println("Please enter 2 for 2D, 3 for 3D or 4 for 4D");
+				int dimension = input.nextInt();
+				System.out.println("n\tR^2\t\t\t\tF_SAW");
 				
-				for (int n = 10; n <= Globals.max_steps; n++) {
-					rSquaredSum = new CopyOnWriteArrayList<Double>();
-					N_SAW_TOT = new AtomicInteger();
-					sumOfAverages = 0;
-					
-					for (int i=0; i<Globals.N_T; i++) {
-						Thread t1 = new Thread(new Dimensions(dimension, n));
-						t1.start();
-						arrThreads4D[i] = t1;
+					if (dimension == 2) {
+						Thread arrThreads2D[] = new Thread[Globals.N_T];
+			
+						for (int n = 10; n <= Globals.max_steps; n++) {
+		
+							rSquaredSum = new CopyOnWriteArrayList<Double>();
+							N_SAW_TOT = new AtomicInteger();
+							sumOfAverages = 0;
+							
+							for (int i=0; i<Globals.N_T; i++) {
+								Thread t1 = new Thread(new Dimensions(dimension, n));
+								t1.start();
+								arrThreads2D[i] = t1;
+							}
+							for (int i = 0; i < Globals.N_T; i++) {
+								arrThreads2D[i].join();
+							}
+							print(n);
+						}
 					}
-					for (int i = 0; i < Globals.N_T; i++) {
-						arrThreads4D[i].join();
+					else if (dimension == 3) {
+						Thread arrThreads3D[] = new Thread[Globals.N_T];
+			
+						for (int n = 10; n <= Globals.max_steps; n++) {
+							rSquaredSum = new CopyOnWriteArrayList<Double>();
+							N_SAW_TOT = new AtomicInteger();
+							sumOfAverages = 0;
+							
+							for (int i=0; i<Globals.N_T; i++) {
+								Thread t1 = new Thread(new Dimensions(dimension, n));
+								t1.start();
+								arrThreads3D[i] = t1;
+							}
+							for (int i = 0; i < Globals.N_T; i++) {
+								arrThreads3D[i].join();
+							}
+							print(n);
+						}
 					}
-					print(n);
-				}
+					else if (dimension == 4) {
+						Thread arrThreads4D[] = new Thread[Globals.N_T];
+						
+						for (int n = 10; n <= Globals.max_steps; n++) {
+							rSquaredSum = new CopyOnWriteArrayList<Double>();
+							N_SAW_TOT = new AtomicInteger();
+							sumOfAverages = 0;
+							
+							for (int i=0; i<Globals.N_T; i++) {
+								Thread t1 = new Thread(new Dimensions(dimension, n));
+								t1.start();
+								arrThreads4D[i] = t1;
+							}
+							for (int i = 0; i < Globals.N_T; i++) {
+								arrThreads4D[i].join();
+							}
+							print(n);
+						}
+					}
+			}
+			else if (choice.equalsIgnoreCase("p")) {
+				System.out.println("Please enter 2 for 2D Polygon, 3 for 3D Polygon or 4 for 4D Polygon");
+				int dimension = input.nextInt();
+				System.out.println("n\tF_SAW");
+				
+					if (dimension == 2) {
+						Thread arrThreads5D[] = new Thread[Globals.N_T];
+						
+						for (int n = 2; n <= Globals.max_steps; ) {
+							N_SAW_TOT = new AtomicInteger();
+							sumOfAverages = 0;
+							
+							for (int i=0; i<Globals.N_T; i++) {
+								Thread t1 = new Thread(new Polygon(dimension, n));
+								t1.start();
+								arrThreads5D[i] = t1;
+							}
+							for (int i = 0; i < Globals.N_T; i++) {
+								arrThreads5D[i].join();
+							}
+							printPoly(n);
+							n +=2;
+						}
+					}
+					else if (dimension == 3) {
+						Thread arrThreads5D[] = new Thread[Globals.N_T];
+						
+						for (int n = 2; n <= Globals.max_steps; ) {
+							N_SAW_TOT = new AtomicInteger();
+							sumOfAverages = 0;
+							
+							for (int i=0; i<Globals.N_T; i++) {
+								Thread t1 = new Thread(new Polygon(dimension, n));
+								t1.start();
+								arrThreads5D[i] = t1;
+							}
+							for (int i = 0; i < Globals.N_T; i++) {
+								arrThreads5D[i].join();
+							}
+							printPoly(n);
+							n +=2;
+						}
+					}
+					else if (dimension == 4) {
+						Thread arrThreads5D[] = new Thread[Globals.N_T];
+						
+						for (int n = 2; n <= Globals.max_steps; ) {
+							N_SAW_TOT = new AtomicInteger();
+							sumOfAverages = 0;
+							
+							for (int i=0; i<Globals.N_T; i++) {
+								Thread t1 = new Thread(new Polygon(dimension, n));
+								t1.start();
+								arrThreads5D[i] = t1;
+							}
+							for (int i = 0; i < Globals.N_T; i++) {
+								arrThreads5D[i].join();
+							}
+							printPoly(n);
+							n +=2;
+						}
+					}
 			}
 			System.out.println("Enter y to continue: ");
 			String st = input.next();
@@ -88,6 +155,7 @@ public class Main {
 		}
 		input.close();
 	}
+
 	public static void print(int n) {
 		rSquaredSum.forEach(a -> sumOfAverages += a);
 		rsqavg = sumOfAverages/ N_SAW_TOT.doubleValue();
@@ -97,6 +165,12 @@ public class Main {
 			System.out.print(rsqavg+"\t\t");
 			System.out.println(f_SAW);
 		}
+	}
+	
+	public static void printPoly(int n) {
+		double f_SAW = N_SAW_TOT.doubleValue()/(Globals.N_T * Globals.N_W);	
+		System.out.print(n+"\t");
+		System.out.println(f_SAW);
 	}
 }
 
