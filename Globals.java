64203f8704d5c9dbfcfcf77ff1ef08db345828e1
SyedHussain <tah94mid@gmail.com>
2019-10-15 18:55:48 -0400

    3

--- Globals.java
+++ Globals.java
@@ -2,8 +2,8 @@ package sawr;
 
 public final class Globals {
 	static int max_steps = 40; // max # of steps
-	static int N_W = 1000000;  // # of walk 
-	static int N_T = 10;    // # of thread
+	static int N_W = 100000;  // # of walk 
+	static int N_T = 100;    // # of thread
 }
 
 
